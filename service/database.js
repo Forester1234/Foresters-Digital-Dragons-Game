d6ec0aabfcfe294b9e4b8c41744f487355e69ece
Forester1234 <jfc82@byu.edu>
2026-03-24 13:29:03 -0600

    added test connection

--- service/database.js
+++ service/database.js
@@ -6,3 +6,13 @@ const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
 const gamesCollection = db.collection('games');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
