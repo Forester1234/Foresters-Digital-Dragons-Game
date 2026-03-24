6b34c55d87e831e614d93eb7c086365a90100168
Forester1234 <jfc82@byu.edu>
2026-03-24 13:38:29 -0600

    Added helper functions

--- service/database.js
+++ service/database.js
@@ -5,7 +5,7 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
-const gamesCollection = db.collection('games');
+const gameCollection = db.collection('games');
 
 (async function testConnection() {
   try {
@@ -16,3 +16,42 @@ const gamesCollection = db.collection('games');
     process.exit(1);
   }
 })();
+
+// USERS
+function getUser(email) {
+  return userCollection.findOne({ email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function removeUserToken(user) {
+  await userCollection.updateOne({ email: user.email }, { $unset: { token: '' } });
+}
+
+// GAMES
+async function addGame(game) {
+  await gameCollection.insertOne(game);
+}
+
+async function updateGame(game) {
+  await gameCollection.updateOne({ name: game.name }, { $set: game }, { upsert: true });
+}
+
+function getGame(name) {
+  return gameCollection.findOne({ name });
+}
+
+function getAllGames() {
+  return gameCollection.find({}).toArray();
+}
+
